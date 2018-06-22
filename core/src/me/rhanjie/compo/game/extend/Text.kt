aea73aaeac840d1a48026ce2c9336be557a64223
RhAnjiE <iRhAnjiE@gmail.com>
2018-06-22 22:32:57 +0200

    Extended Text class - Fonts support - Multiline support - Align support

--- core/src/me/rhanjie/compo/game/extend/Text.kt
+++ core/src/me/rhanjie/compo/game/extend/Text.kt
@@ -2,17 +2,67 @@ package me.rhanjie.compo.game.extend
 
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Actor
 
-class Text: Actor() {
-    var font: BitmapFont = BitmapFont()
-    var text: String = ""
+enum class Align {
+    LEFT, CENTER, RIGHT
+}
 
-    fun update(text: String){
-        this.text = text
+class Text constructor(position: Vector2, private var textLines: ArrayList<String> = arrayListOf(""),
+                       private val font: BitmapFont = BitmapFont(), private var align: Align = Align.LEFT): Actor() {
+
+    private var offset: Float = 0F
+
+    init {
+        x = position.x
+        y = position.y
+
+        this.updateOffset()
+    }
+
+    fun changeText(textLines: ArrayList<String>){
+        this.textLines = textLines
+
+        var longestLine = 0
+        for(text in textLines){
+            if (longestLine < text.length)
+                longestLine = text.length
+        }
+
+        height = font.lineHeight
+        width = longestLine * height
+
+        this.updateOffset()
+    }
+
+    //TODO: Setting color doesn't work
+    fun changeColor(r: Int, g: Int, b: Int, a: Int){
+        font.setColor(r / 255F, g / 255F, b / 255F, a / 255F)
+    }
+
+    fun changeScale(scale: Float){
+        font.data.setScale(scale)
+    }
+
+    fun setAlign(align: Align){
+        this.align = align
+
+        this.updateOffset()
+    }
+
+    private fun updateOffset() {
+        offset = when(align){
+            Align.LEFT   -> 0F
+            Align.CENTER -> width / 2
+            Align.RIGHT  -> width
+        }
+
+        println(offset)
     }
 
     override fun draw(batch: Batch?, parentAlpha: Float) {
-        font.draw(batch, text, x, y)
+        for(i in textLines.indices)
+            font.draw(batch, textLines[i], x - offset, y - (height * i))
     }
 }
--- core/src/me/rhanjie/compo/game/ui/Hud.kt
+++ core/src/me/rhanjie/compo/game/ui/Hud.kt
@@ -3,9 +3,12 @@ package me.rhanjie.compo.game.ui
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
 import me.rhanjie.compo.game.characters.Player
+import me.rhanjie.compo.game.extend.Align
 import me.rhanjie.compo.game.extend.Text
 import me.rhanjie.compo.game.resources.TexturesManager
 
@@ -13,22 +16,25 @@ import me.rhanjie.compo.game.resources.TexturesManager
 class Hud {
     companion object {
         var stage: Stage = Stage()
-        var scoreLabel: Text = Text()
+        var scoreLabel: Text = Text(Vector2(0F, 0F), arrayListOf("Score: 0"), BitmapFont(Gdx.files.internal("font.fnt")), Align.CENTER)
 
         var score: Int = 0
+        var hunger: Int = 0
 
         fun create() {
             score = 0
+            hunger = 0
 
-            scoreLabel.text = "Score: "
             scoreLabel.x = Gdx.graphics.width / 2F
-            scoreLabel.y = 100F
+            scoreLabel.y = 70F
+            scoreLabel.changeColor(255, 255, 0, 150)
 
             stage.addActor(scoreLabel)
         }
 
         fun update(){
-            scoreLabel.text = "Score: $score"
+            //TODO: Add ui bar for hunger
+            scoreLabel.changeText(arrayListOf("Score: $score", "Hunger: $hunger/100"))
         }
 
         fun render() {
